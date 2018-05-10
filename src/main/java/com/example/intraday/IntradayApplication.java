f78c2e710d9813ecde2a0ddd1d1b305a255b3699
iVesperX <vesperx9@outlook.com>
2018-05-10 16:18:31 -0400

    added spring boot project

---
+++ src/main/java/com/example/intraday/IntradayApplication.java
@@ -0,0 +1,12 @@
+package com.example.intraday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntradayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntradayApplication.class, args);
+	}
+}
