d47ae232d0fc47cd2cdc6e2d6b308de2370bca97
senorcl0ud <claudemcalpin@gmail.com>
2018-05-11 13:24:20 -0400

    refactored timeseries class

--- src/main/java/com/example/intraday/TimeSeries.java
+++ src/main/java/com/example/intraday/TimeSeries.java
@@ -1,59 +1,17 @@
 package com.example.intraday;
 
+
+import java.sql.Time;
+
 public class TimeSeries {
-    private double timestamp;
-    private double open;
-    private double high;
-    private double low;
-    private double close;
+   private Time time;
 
-    public double getTimestamp() {
-        return timestamp;
+   
+    public Time getTime() {
+        return time;
     }
 
-    public void setTimestamp(double timestamp) {
-        this.timestamp = timestamp;
+    public void setTime(Time time) {
+        this.time = time;
     }
-
-    public double getOpen() {
-        return open;
-    }
-
-    public void setOpen(double open) {
-        this.open = open;
-    }
-
-    public double getHigh() {
-        return high;
-    }
-
-    public void setHigh(double high) {
-        this.high = high;
-    }
-
-    public double getLow() {
-        return low;
-    }
-
-    public void setLow(double low) {
-        this.low = low;
-    }
-
-    public double getClose() {
-        return close;
-    }
-
-    public void setClose(double close) {
-        this.close = close;
-    }
-
-    public double getVolume() {
-        return volume;
-    }
-
-    public void setVolume(double volume) {
-        this.volume = volume;
-    }
-
-    private double volume;
 }
