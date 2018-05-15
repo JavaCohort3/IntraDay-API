985467fa999853e4d41000253d91e9cd19c169cb
NightShadow3000 <RoyalFotoK@gmail.com>
2018-05-15 17:41:29 -0400

    I made the changes

--- src/main/java/com/example/intraday/IntraDay.java
+++ src/main/java/com/example/intraday/IntraDay.java
@@ -2,11 +2,14 @@ package com.example.intraday;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class IntraDay {
 
     private MetaData metaData;
 
-    private TimeSeries timeSeries;
+    private Map<Integer, Time> timeSeries = new TreeMap<>();
 
     private IntraDay() { }
 
@@ -14,28 +17,28 @@ public class IntraDay {
     public MetaData getMetaData() {
         return metaData;
     }
+
     @JsonProperty("Meta Data")
     public void setMetaData(MetaData metaData) {
         this.metaData = metaData;
     }
-  
+
     @JsonProperty("Time Series (1min)")
-    public TimeSeries getTimeSeries() {
+    public Map<Integer, Time> getTimeSeries() {
         return timeSeries;
     }
+
     @JsonProperty("Time Series (1min)")
-    public void setTimeSeries(TimeSeries timeSeries) {
+    public void setTimeSeries(Map<Integer, Time> timeSeries) {
         this.timeSeries = timeSeries;
     }
 
-
     @Override
     public String toString() {
-        return "com.example.intraday.IntraDay{" +
+        return "IntraDay{" +
                 "metaData=" + metaData +
                 ", timeSeries=" + timeSeries +
                 '}';
     }
-
 
 }
