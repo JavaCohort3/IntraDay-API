bf5f7b0682e3c85f7491f30793a7609de9f65c3f
aermias <37191261+aermias@users.noreply.github.com>
2018-05-16 10:59:11 -0400

    fixed claude bs

--- src/main/java/com/example/intraday/IntradayApplication.java
+++ src/main/java/com/example/intraday/IntradayApplication.java
@@ -9,19 +9,11 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Map;
 import java.util.TreeMap;
 
 @SpringBootApplication
-public class IntradayApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(IntradayApplication.class, args);
-
-
-    }
-	
+public class IntradayApplication {	
 	private static final Logger log = LoggerFactory.getLogger(SpringApplication.class);
 	private static RestTemplateBuilder builder = new RestTemplateBuilder();
 
